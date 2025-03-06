22cdd6035caad84a5c4dd471523b86684430be42
Emilien <emilien.giraud@gottaphish.com>
2025-03-06 18:35:27 +0100

    Ajout de redirections dans la configuration Next.js et création du fichier de configuration Vercel.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+ async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/Homepage',
+        permanent: true, // mettez true si c'est une redirection permanente
+      },
+    ]
+  },
   reactStrictMode: true,
   // autres configurations
 };
